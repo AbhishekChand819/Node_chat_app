baef3b18b35243b65ff9e94d54b7a7b54bfe0b26
Abhishek <abhishekchand055@gmail.com>
2019-03-02 12:09:03 +0530

    Geolocation API

--- public/js/index.js
+++ public/js/index.js
@@ -15,6 +15,16 @@ socket.on('newMessage', (message) => {
     jQuery('#messages').append(li);
 }); 
 
+socket.on('newLocationMessage',(message) => {
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My current Location</a>');
+    
+    li.text(`${message.from}:  `);
+    a.attr('href',message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
+})
+
 jQuery('#message-form').on('submit',(e) => {
     e.preventDefault();
     socket.emit('createMessage',{
@@ -22,5 +32,20 @@ jQuery('#message-form').on('submit',(e) => {
         text:jQuery('[name=message]').val()
     },()=>{
 
+    });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', () => {
+    if(!navigator.geolocation) {
+        return alert('Geolocation not supported by your browser.')
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    }, () => {
+        alert('Unable to fetch Location. ');
     });
 });

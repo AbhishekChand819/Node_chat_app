173c7caefffdee5a61dc7d319559d630a808d625
Abhishek <abhishekchand055@gmail.com>
2019-03-02 12:46:18 +0530

    Styling

--- public/js/index.js
+++ public/js/index.js
@@ -27,11 +27,13 @@ socket.on('newLocationMessage',(message) => {
 
 jQuery('#message-form').on('submit',(e) => {
     e.preventDefault();
+
+    var messageTextbox = jQuery('[name=message]');
     socket.emit('createMessage',{
         from:'User',
-        text:jQuery('[name=message]').val()
+        text:messageTextbox.val()
     },()=>{
-
+        messageTextbox.val('')
     });
 });
 
@@ -40,12 +42,17 @@ locationButton.on('click', () => {
     if(!navigator.geolocation) {
         return alert('Geolocation not supported by your browser.')
     }
+
+    locationButton.attr('disabled','disabled').text('Sending Location...');
+
     navigator.geolocation.getCurrentPosition((position) => {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
-        })
+        });
     }, () => {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch Location. ');
     });
 });
